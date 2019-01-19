1cc7a6fcdf671425fa7462a55ed96e66338a675f
System Administrator <root@bens-mbp.lan>
2019-01-19 16:31:39 -0600

    Add tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,10 +4,16 @@ const request = require('supertest');
 var {app} = require('./../server');
 var {Todo} = require('./../models/todo');
 
+const todos = [{
+  text: 'First test to do'
+}, {
+  text: 'Second test todo'
+}]
+
 beforeEach((done) => {
   Todo.deleteMany({}).then(() => {
-    done();
-  })
+    return Todo.insertMany(todos);
+  }).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -26,7 +32,7 @@ describe('POST /todos', () => {
           return done(err);
         }
 
-        Todo.find().then((todos) => {
+        Todo.find({text}).then((todos) => {
           expect(todos.length).toBe(1);
           expect(todos[0].text).toBe(text);
           done();
@@ -47,11 +53,23 @@ describe('POST /todos', () => {
         }
 
         Todo.find().then((docs) => {
-          expect(docs.length).toBe(0);
+          expect(docs.length).toBe(2);
           done();
         }).catch((e) => {
           done(e)
         });
       });
+  });
+});
+
+describe('GET /todos route', () => {
+  it('should get all todos', (done) => {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todos.length).toBe(2);
+      })
+      .end(done);
   });
 });
