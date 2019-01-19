0cf51dad4989a1ecfbb1737d4acc890c5d6119e3
System Administrator <root@bens-mbp.lan>
2019-01-19 17:51:41 -0600

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -28,7 +29,29 @@ app.get('/todos', (req, res) => {
     res.send({todos})
   }, (e) => {
     res.status(400).send(e);
-  });  
+  });
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if(!ObjectID.isValid(id)) {
+    res.status(404).send();
+    console.log('invalid object id')
+  }
+
+  Todo.findById(id).then((todo) => {
+    if(!todo) {
+      res.status(404).send()
+    }
+    res.send({todo})
+    console.log('todo : ', JSON.stringify(todo, undefined,2));
+
+
+  }).catch((e) => {
+    res.status(400).send();
+  });
+  
 });
 
 app.listen(3000, () => {
