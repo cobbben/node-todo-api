37753ed44a7f545c6b074479fed53447ab7d9737
System Administrator <root@bens-mbp.lan>
2019-01-20 09:44:09 -0600

    Add test cases for GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -37,7 +37,7 @@ app.get('/todos/:id', (req, res) => {
 
   if(!ObjectID.isValid(id)) {
     res.status(404).send();
-    console.log('invalid object id')
+    //console.log('invalid object id')
   }
 
   Todo.findById(id).then((todo) => {
@@ -45,13 +45,11 @@ app.get('/todos/:id', (req, res) => {
       res.status(404).send()
     }
     res.send({todo})
-    console.log('todo : ', JSON.stringify(todo, undefined,2));
-
-
+    //console.log('todo : ', JSON.stringify(todo, undefined,2));
   }).catch((e) => {
     res.status(400).send();
   });
-  
+
 });
 
 app.listen(3000, () => {
